ee586e9973635257274b923325c2b0088c3d1f2f
SergejDK <sedic001@stud.fh-dortmund.de>
2019-06-18 01:06:02 +0200

    rn native

--- ReactNativeCompareApplication/App.js
+++ ReactNativeCompareApplication/App.js
@@ -6,8 +6,11 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { View, Platform, StyleSheet } from 'react-native';
+import NavigationDrawerStructure, { createDrawerNavigator,createAppContainer, createStackNavigator } from 'react-navigation'
+import { Button,Container,Header,Left,Right,Icon,Text } from 'native-base';
+import NativeScreen from './screens/nativescreen';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,34 +19,82 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+class MyHomeScreen extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left style={{ flexDirection: 'row'}}>
+           <Icon onPress={() => this.props.navigation.openDrawer()} name="md-menu" style={{ color: 'white', marginRight: 15 }} />
+          </Left>
+          <Right>
+           <Icon name="md-cart" style={{ color: 'white' }} />
+          </Right>
+        </Header>
+       <View style={{ marginTop:100,marginLeft:100}}>
+         <Button onPress={() => this.props.navigation.navigate('Notifications')} >
+           <Text>Go to notifications</Text>
+         </Button>
+        </View>
+       </Container>
+     );
+   }
+ }
+
+
+class MyNotificationsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ marginTop:100,marginLeft:100}}>
+        <Button onPress={() => this.props.navigation.goBack()} >
+          <Text>Go back home</Text>
+        </Button>
+      </View>
+    );
+   }
+}
+
+const Native_StackNavigator = createStackNavigator({
+  //All the screen from the Screen3 will be indexed here
+  Third: {
+    screen: NativeScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Native Screen',
+      //headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF9800',
+      },
+      headerTintColor: '#fff',
+      headerMode: 'screen',
+    }),
+  },
+});
+
+const MyDrawerNavigator = createDrawerNavigator({
+  Home:{ 
+     screen: MyHomeScreen,
+  },
+  Notifications: {
+     screen: MyNotificationsScreen,
+     navigationOptions: {
+       drawerLabel: 'Notifications'
+     }
+  },
+  Native: {
+    screen: Native_StackNavigator,
+  }
+});
+
+const MyApp = createAppContainer(MyDrawerNavigator);
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <MyApp></MyApp >
+      </Container>
     );
   }
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
