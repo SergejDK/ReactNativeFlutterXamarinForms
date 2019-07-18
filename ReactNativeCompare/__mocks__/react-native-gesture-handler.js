b05fa2317c6f96c5091704a2ea4d704cbcfec179
SergejDK <sedic001@stud.fh-dortmund.de>
2019-07-19 00:21:12 +0200

    some fixes

---
+++ ReactNativeCompare/__mocks__/react-native-gesture-handler.js
@@ -0,0 +1,31 @@
+const View = require('react-native/Libraries/Components/View/View');
+export default {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+}
--- ReactNativeCompare/__tests__/App-test.js
+++ ReactNativeCompare/__tests__/App-test.js
@@ -5,9 +5,11 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
+import { NativeModules } from "react-native";
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+NativeModules.RNGestureHandlerModule = {};
 
 it('renders correctly', () => {
   const tree = renderer.create(<App />).toJSON();
